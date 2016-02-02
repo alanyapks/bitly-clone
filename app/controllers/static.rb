dda1af8d8d68ec1313c4212f13e347394becff68
Next <nextacademy@email.com>
2016-02-01 23:10:19 -0800

    Added create new url method

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,17 @@
+require 'sinatra'
+
 get '/' do
+	@urls = Url.all
   erb :"static/index"
+end
+
+post '/urls' do
+	# create new url
+	@new_url = Url.new(long_url: params[:long_url]).save
+end
+
+get '/:short_url' do
+	Url.shorten 
+	# Redirect to appropriate "Long" url
+	redirect '/urls'
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,11 @@
+require 'securerandom'
+
 class Url < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
+	def shorten
+		# Url.to_a
+		# Array.new(7){['a'..'z', 'A'..'Z','0'..'9'].sample}
+		self.shorten = SecureRandom.hex(7)
+		
+	end
 end
